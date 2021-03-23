cf582b8133542b15acba3387bdafcf1a2b3ba243
ademcakmak <adem.cakmak@globit.com.tr>
2021-03-23 15:15:47 +0300

    using template keyword PART 2

--- src/app/app-servers/app-servers.component.ts
+++ src/app/app-servers/app-servers.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  template:
+ `<app-server></app-server>
+  <app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./app-servers.component.css']
 })
 export class AppServersComponent implements OnInit {
