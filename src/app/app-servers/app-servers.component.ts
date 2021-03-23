d7de76fac24143343b18074d5bb882466433fd15
ademcakmak <adem.cakmak@globit.com.tr>
2021-03-23 15:26:52 +0300

    styles css

--- src/app/app-servers/app-servers.component.ts
+++ src/app/app-servers/app-servers.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template:
- `<app-server></app-server>
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl : 'app-servers.component.html',
   styleUrls: ['./app-servers.component.css']
 })
 export class AppServersComponent implements OnInit {
