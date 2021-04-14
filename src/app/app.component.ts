18449a734d497ac19f99592f5bd3c9979a11234f
adem.cakmak <adem.cakmak@globit.com.tr>
2021-04-14 11:41:52 +0300

    event emitter output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Test Server', content: 'Content of server'}];
+
+    onServerAdded(serverData: {sname: string, scontent: string}) {
+        this.serverElements.push({
+          type: 'server',
+          name: serverData.sname,
+          content: serverData.scontent
+        });
+    }
+
+    onBlueprintAdded(blueprintData: {sname: string, scontent: string}) {
+        this.serverElements.push({
+          type: 'blueprint',
+          name: blueprintData.sname,
+          content: blueprintData.scontent
+        });
+    }
+
 }
