544f1647f54b1da383f063853c95149da37031b3
adem.cakmak <adem.cakmak@globit.com.tr>
2021-04-22 23:44:01 +0300

    assignment 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,17 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.Emulated // Emulated, ShadowDom
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'Test Server', content: 'Content of server'}];
 
-    onServerAdded(serverData: {soname: string, content: string}) {
-        this.serverElements.push({
-          type: 'server',
-          name: serverData.soname,
-          content: serverData.content
-        });
-    }
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
 
-    onBlueprintAdded(blueprintData: {soname: string, content: string}) {
-        this.serverElements.push({
-          type: 'blueprint',
-          name: blueprintData.soname,
-          content: blueprintData.content
-        });
-    }
+  onIntervalFired(firedNum: number) {
+    if ( firedNum % 2 === 0 ) { this.oddNumbers.push(firedNum); } else { this.evenNumbers.push(firedNum); }
+  }
 
 }
