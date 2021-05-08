4746c13db7ea81d12f92e6139c3371d1a0be52ae
adem.cakmak <adem.cakmak@globit.com.tr>
2021-05-08 17:21:18 +0300

    observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from './user.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  private activatedFlag = false;
+  private subscription: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.subscription = this.userService.activatedEmitter.subscribe((flag)=>{
+      this.activatedFlag = flag;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {interval, Subscription, Observable} from 'rxjs';
-import {count} from 'rxjs/operators';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -25,12 +25,27 @@ export class HomeComponent implements OnInit, OnDestroy {
           let count = 0;
           setInterval(() =>{
             observer.next(count++);
+            if( count === 2 )
+              observer.complete()
+            if( count > 3 )
+              observer.error(new Error('count is greater than 3'));
           }, 1000);
         }
+    ).pipe(
+        filter((data: number) => {
+          return data > 0;
+        }),
+        map((data)=>{
+          return 'Round: '+(data+1);
+        })
     );
 
     this.customIntervalSubscription = customInterval.subscribe((data)=>{
       console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('completed');
     })
   }
 
