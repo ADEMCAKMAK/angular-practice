31f6828ff6b8fb2975f19359bd83000e24edab2a
adem.cakmak <adem.cakmak@globit.com.tr>
2021-05-09 22:28:34 +0300

    form assingment 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,51 +8,18 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
 
-  @ViewChild('htmlFormElement') signupForm: NgForm;
-  defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    gender: ''
-  };
-
   submitted = false;
 
-  suggestUserName() {
-/*    const suggestedName = 'Superuser';
-    this.signupForm.setValue({
-      userData : {
-        username: 'adem',
-        email: ''
-      },
-      secret: 'pet',
-      questionAnswer: 'milo',
-      gender: 'male'
-    });*/
-    this.signupForm.form.patchValue({
-      userData : {
-        username: 'adem',
-        email: ''
-      }
-    });
-  }
+  @ViewChild('saveData') signupForm: NgForm;
+  data = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
 
-/*
-  onSubmit(htmlFormElement: NgForm): void {
-    console.log(htmlFormElement);
-  }
-*/
-
-
-  onSubmit(): void {
-    console.log(this.signupForm);
+  onSave() {
+    console.log(this.signupForm.value);
     this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.email;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
+    this.data = this.signupForm.value;
   }
 }
