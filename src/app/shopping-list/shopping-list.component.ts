74ca41d2897167ea3f91ed38ac442e1c793a75df
adem.cakmak <adem.cakmak@globit.com.tr>
2021-05-13 23:29:01 +0300

    template driven form

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -28,4 +28,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
+
+    onEditItem(i: number) {
+        this.slService.startEditing.next(i);
+    }
 }
